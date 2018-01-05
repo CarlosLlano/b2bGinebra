bec5c2190a56a5854daa8399bdc0f44b8d934ad9
Carlos <Carlos@192.168.0.8>
2018-01-05 02:39:44 -0500

    cambios

--- b2bGinebra/deployment/postgres-database/scriptsDB/3.secuencias.sql
+++
@@ -1,8 +0,0 @@
-DO $$
-DECLARE 
-i TEXT;
-BEGIN
-    FOR i IN (select table_name from information_schema.tables where table_catalog='postgres' and table_schema='public') LOOP
-    EXECUTE 'CREATE SEQUENCE SEQ_'||i|| ' minvalue 1 start with 1 increment by 1;';
-    END LOOP;
-END$$;
